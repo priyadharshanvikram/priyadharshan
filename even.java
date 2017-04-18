91dff8a58ef813f6e5d8fc2fd13dcba9d7128ac7
priyadharshanvikram <priyadharshanvikram@gmail.com>
2017-04-18 15:19:20 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,26 @@
+package check;
+
+import java.util.Scanner;
+
+public class even {
+	public static void main(String args[])
+	{
+		int a;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("number is even");
+		}
+		else if(a%2=!0)
+		{
+			System.out.println("number is odd");
+		}
+             else
+                   {
+                        System.out.println("not valid");
+                   }
+	}
+
+
+}
