94d7467d27499f17c8d69219644595c8caee5672
priyadharshanvikram <priyadharshanvikram@gmail.com>
2017-06-06 15:49:36 +0530

    Add files via upload

---
+++ large.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class large{
+public static void main(String[] args)
+{
+int b,c=0;
+Scanner s=new Scanner(System.in);
+System.out,println("Enter the numbers:");
+int a=s.nextInt();
+for(i=1;i<4:i++)
+{
+b=s.nextInt();
+if(b>c)
+{
+c=b;
+}
+}
+}
+}
